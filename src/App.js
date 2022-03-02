53a23d921245e0417592e973d22430cc7404f64e
Alexander Paule <alec.paule@yahoo.com>
2022-03-02 12:47:17 -0800

    Adds guessing word and selecting letters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.scss";
 
 function App() {
+  const [letter, setLetter] = useState("");
+  const [selectedLetters, setSelectedLetters] = useState([]);
+
+  const word = "hello";
+  let charList = [];
+  let j = 0;
+  [...word].forEach((char) =>
+    charList.push(<li key={j++}>{char.toUpperCase()}</li>)
+  );
+
   let buttonList = [];
   for (let i = 65; i < 91; i++) {
     buttonList.push(
-      <button key={i} onClick={() => console.log(String.fromCharCode(i))}>
+      <button
+        className={
+          "letterButton " +
+          (selectedLetters.includes(String.fromCharCode(i).toUpperCase())
+            ? "isSelected"
+            : "")
+        }
+        key={i}
+        onClick={() =>
+          setSelectedLetters([
+            ...selectedLetters,
+            String.fromCharCode(i).toUpperCase(),
+          ])
+        }
+      >
         {String.fromCharCode(i)}
       </button>
     );
@@ -13,6 +37,7 @@ function App() {
 
   return (
     <div className="App">
+      <ul className="word">{charList}</ul>
       <ul className="buttons">{buttonList}</ul>
     </div>
   );
